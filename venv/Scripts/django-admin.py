36fc24d21262adecdd02d0f164ee6bcc94caf6be
HitechPrjects <youremail@hitechsolutionsproject@gmail.com>
2025-10-15 11:32:37 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\L E N O V O\Downloads\Django-librarymanagement-main\Django-librarymanagement-main\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
